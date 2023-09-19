9eec34aaf0e898ed4164fe5293f5cde8f2e8b13d
Deeksha Rungta <deeksharu1902@gmail.com>
2023-09-19 13:29:47 +0530

    fix: Make canvas responsive #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import './App.css';
-import { useDraw } from './hooks/useDraw';
-import { drawLine } from './hooks/drawline';
-import Sidebar from './component/sidebar';
-import { useColorProvider } from './state/colorProvider';
-import Navbar from './component/navbar';
-import { useState } from 'react';
-import { useThemeProvider } from './state/themeProvider'
+import "./App.css";
+import { useDraw } from "./hooks/useDraw";
+import { drawLine } from "./hooks/drawline";
+import Sidebar from "./component/sidebar";
+import { useColorProvider } from "./state/colorProvider";
+import Navbar from "./component/navbar";
+import { useEffect, useState } from "react";
+import { useThemeProvider } from "./state/themeProvider";
 
 function App() {
   const { toggleTheme } = useThemeProvider();
   const [isOpen, setIsOpen] = useState(false);
+  const [canvasWidth, setCanvasWidth] = useState(window.innerWidth * 0.8);
 
   const { color, setColor } = useColorProvider();
   const onDrawWrapper = (drawProps: any) => {
@@ -17,28 +18,57 @@ function App() {
   };
   const { canvasRef, onMouseDown, clear } = useDraw(onDrawWrapper, setColor);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setCanvasWidth(window.innerWidth * 0.8);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className='flex flex-col h-screen bg-white'>
-      <div className='h-[8%]'>
+    <div className="flex flex-col h-screen bg-white">
+      <div className="h-[8%]">
         <Navbar setIsOpen={setIsOpen} />
       </div>
-      <div className={`relative flex-1 flex justify-between items-center ${toggleTheme ? 'bg-[#1D1D1D] text-white' : 'bg-white text-black'}`}>
-        <div className='border border-black rounded-md ml-8 border-opacity-30 w-[80%] largeTablet:w-[100%]'>
-          <div className='border-b border-black border-opacity-25'>
-            <button type='button' className={`${toggleTheme ? 'p-2 rounded-md border-r border-b hover:bg-black border-black' : 'p-2 rounded-md border-r border-b hover:bg-gray-100 border-black'}`} onClick={clear}>
+      <div
+        className={`relative flex-1 flex justify-between items-center ${
+          toggleTheme ? "bg-[#1D1D1D] text-white" : "bg-white text-black"
+        }`}
+      >
+        <div className="border border-black rounded-md ml-8 border-opacity-30">
+          <div className="border-b border-black border-opacity-25">
+            <button
+              type="button"
+              className={`${
+                toggleTheme
+                  ? "p-2 rounded-md border-r border-b hover:bg-black border-black"
+                  : "p-2 rounded-md border-r border-b hover:bg-gray-100 border-black"
+              }`}
+              onClick={clear}
+            >
               Clear canvas
             </button>
           </div>
           <canvas
-            id='canvas'
-            width={1200}
+            width={canvasWidth}
             height={600}
             ref={canvasRef}
             onMouseDown={onMouseDown}
-            className={`border-opacity-30 ${toggleTheme && 'bg-white'}`}
+            className={`border-opacity-30 ${toggleTheme && "bg-white"}`}
           />
         </div>
-        <div className={`${toggleTheme && 'bg-[#2D2D2D]'} ${isOpen ? 'fixed inset-y-0 right-0 bg-white h-screen w-[20%] z-50' : 'h-full w-[15%]'}`}>
+        <div
+          className={`${toggleTheme && "bg-[#2D2D2D]"} ${
+            isOpen
+              ? "fixed inset-y-0 right-0 bg-white h-screen w-[20%] z-50"
+              : "h-full w-[15%]"
+          }`}
+        >
           <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
         {isOpen && (
@@ -49,7 +79,7 @@ function App() {
         )}
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default App;
